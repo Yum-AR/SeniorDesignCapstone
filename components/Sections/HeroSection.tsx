6b86a34cc4d82aa5f822e32ca83c82d853419c0d
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 15:56:43 -0400

    refactored hero section

--- components/Sections/HeroSection.tsx
+++ components/Sections/HeroSection.tsx
@@ -1,34 +1,8 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useEffect, useState } from 'react'
-//import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import SearchBar from '../Functionality/SearchBar'
-import { useRouter } from 'next/router'
-import { useAuth } from '../../firebase/AuthContext'
+import React from 'react'
 import HomeNav from '../Nav/HomeNav'
+import SearchBar from '../Functionality/SearchBar'
 
-export default function HeroSection() {
-  const [Error, setError] = useState({})
-  const [showAuthModal, setAuthModal] = useState(false);
-  const [showSignUpModal, setSignUpModal] = useState(false);
-
-  const { currentUser, logout } = useAuth()
-  const router = useRouter()
-  console.log(currentUser)
-
-  async function signOutHandler() {
-    setError('')
-
-    try {
-      await logout()
-      window.location.reload()
-    } catch {
-      setError("Failed to log out")
-    }
-  }
-  const dashboardPageRedirect = () => {
-    router.push('/dashboard.html')
-  }
-
+const HeroSection: React.FC = () => {
   return (
     <div className="relative bg-gray-50 overflow-hidden">
       <HomeNav className="flex justify-between" />
@@ -39,7 +13,7 @@ export default function HeroSection() {
             <span className=" text-[#FF6F43] xl:inline">mr</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            View your favorite restaurant's menu in mixed reality
+            {'View your favorite restaurant\'s menu in mixed reality'}
           </p>
           <div className="mt-3 mb-[15rem] max-w-md mx-auto md:mt-5 md:text-xl md:max-w-3xl">
             <SearchBar />
@@ -49,5 +23,4 @@ export default function HeroSection() {
     </div >
   )
 }
-
-
+export default HeroSection
