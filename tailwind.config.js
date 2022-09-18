aea10fe1a32308426cea3bc2edce58ecae0e5caa
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 16:54:41 -0400

    updated config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,10 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [
-    //require('@tailwindcss/aspect-ratio'),
-    //require('@tailwindcss/typography'),
-  ],
+  plugins: []
 }
