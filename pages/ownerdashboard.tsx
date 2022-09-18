a794405661d964fb4c2a1b94207612cc60e7d5f3
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 15:57:12 -0400

    refactored owner dashboard

--- pages/ownerdashboard.tsx
+++ pages/ownerdashboard.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from 'react'
 import NavBar from '../components/Nav/NavBar'
-import OwnerDashboardSidebar from '../components/Sections/OwnerDashboardSidebar';
-import MenuEdit from '../components/Sections/MenuEdit';
-import { useAuth } from '../firebase/AuthContext';
-import { useRouter } from 'next/router';
-export default function OwnerDashboard() {
-    const { currentUser } = useAuth()
-    const router = useRouter()
-    return (
+import OwnerDashboardSidebar from '../components/Sections/OwnerDashboardSidebar'
+
+const OwnerDashboard: React.FC = () => {
+  return (
         <div className="h-[100vh]">
             <NavBar />
             <OwnerDashboardSidebar />
 
         </div>
-    )
+  )
 }
+
+export default OwnerDashboard
