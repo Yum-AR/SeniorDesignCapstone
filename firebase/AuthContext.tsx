2d9ebb719498c689e8525037caf2339452efc235
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 17:54:23 -0400

    context refactored

--- firebase/AuthContext.tsx
+++ firebase/AuthContext.tsx
@@ -1,18 +1,17 @@
-import React, { Context, createContext, useContext, useEffect, useState } from 'react'
-import { auth } from './clientApp'
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  onAuthStateChanged,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
   confirmPasswordReset,
-  UserCredential
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  UserCredential,
   // signInWithApple
-} from 'firebase/auth'
-import { String } from 'aws-sdk/clients/batch'
+} from 'firebase/auth';
+import { auth } from './clientApp';
 
 export const AuthContext: React.Context<any> = createContext({
   currentUser: null,
@@ -21,75 +20,68 @@ export const AuthContext: React.Context<any> = createContext({
   registerUser: () => Promise,
   logout: () => Promise,
   forgotPassword: () => Promise,
-  resetPassword: () => Promise
-})
+  resetPassword: () => Promise,
+});
 
-export const useAuth = (): any => useContext(AuthContext)
+export const useAuth = (): any => useContext(AuthContext);
 
 const AuthContextProvider: React.FC<{children: any}> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<any>(null)
-  const [loading, setLoading] = useState(true)
+  const [ currentUser, setCurrentUser ] = useState<any>(null);
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, user => {
-      setCurrentUser((user != null) || null)
-      setLoading(false)
-    })
+      setCurrentUser(user != null || null);
+      setLoading(false);
+    });
     return () => {
-      unsubscribe()
-    }
-  }, [])
+      unsubscribe();
+    };
+  }, []);
 
   useEffect(() => {
-    console.log('The user is', currentUser)
-  }, [currentUser])
+    console.log(`The user is`, currentUser);
+  }, [ currentUser ]);
 
-  async function login (email: string, password: string): Promise<UserCredential> {
-    return await signInWithEmailAndPassword(auth, email, password)
+  async function login(email: string, password: string): Promise<UserCredential> {
+    return await signInWithEmailAndPassword(auth, email, password);
   }
 
-  async function registerUser (email: string, password: string): Promise<UserCredential> {
-    return await createUserWithEmailAndPassword(auth, email, password)
+  async function registerUser(email: string, password: string): Promise<UserCredential> {
+    return await createUserWithEmailAndPassword(auth, email, password);
   }
 
-  async function forgotPassword (email: String): Promise<void> {
+  async function forgotPassword(email: string): Promise<void> {
     return await sendPasswordResetEmail(auth, email, {
-      url: 'http://localhost:3000'
-    })
+      url: `http://localhost:3000`,
+    });
   }
 
-  async function resetPassword (oobCode: String, newPassword: String): Promise<void> {
-    return await confirmPasswordReset(auth, oobCode, newPassword)
+  async function resetPassword(oobCode: string, newPassword: string): Promise<void> {
+    return await confirmPasswordReset(auth, oobCode, newPassword);
   }
 
-  async function logout (): Promise<void> {
-    return await signOut(auth)
+  async function logout(): Promise<void> {
+    return await signOut(auth);
   }
 
-  async function signInWithGoogle (): Promise<UserCredential> {
-    const provider = new GoogleAuthProvider()
-    return await signInWithPopup(auth, provider)
+  async function signInWithGoogle(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return await signInWithPopup(auth, provider);
   }
-  /*
-  export function signInWithApple() {
-      const provider = new firebase.auth.OAuthProvider('apple.com');
-      const result = await auth.signInWithPopup(provider);
-
-      console.log(result.user); // logged-in Apple user
-      return(result)
-  } */
 
   const value = {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     currentUser,
     signInWithGoogle,
     login,
     registerUser,
     logout,
     forgotPassword,
-    resetPassword
-  }
+    resetPassword,
+  };
   return <AuthContext.Provider value={value}>
     {!loading && children}
-  </AuthContext.Provider>
-}
-export default AuthContextProvider
+  </AuthContext.Provider>;
+};
+export default AuthContextProvider;
