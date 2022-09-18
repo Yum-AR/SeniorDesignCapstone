d9163e8da24b58def3c083bb0d20b04e0d85f837
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 17:54:06 -0400

    dashboard refactor

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import NavBar from '../components/Nav/NavBar'
+import NavBar from '../components/Nav/NavBar';
 import DashboardSidebar from '../components/Sections/DashboardSidebar';
-import DashboardTable from '../components/Sections/DashboardTable';
-import { useAuth } from '../firebase/AuthContext';
-import { useRouter } from 'next/router';
 
 export default function dashboard() {
-  const { currentUser } = useAuth()
-  const router = useRouter()
   return (
     <div className="h-[100vh] overflow-hidden">
       <NavBar />
       <DashboardSidebar />
     </div>
-  )
+  );
 }
