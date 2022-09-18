b40d0daf17b9c8fd1cf4dd95ee6250f945a9acbc
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 17:53:57 -0400

    index refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import Head from 'next/head'
-import HeroSection from '../components/Sections/HeroSection'
-import CTA from '../components/Sections/CTA'
-import Features from '../components/Sections/Features'
-import Footer from '../components/Sections/Footer'
-import PhotoOptions from '../components/Sections/PhotoOptions'
+import React from 'react';
+import Head from 'next/head';
+import HeroSection from '../components/Sections/HeroSection';
+import CTA from '../components/Sections/CTA';
+import Features from '../components/Sections/Features';
+import Footer from '../components/Sections/Footer';
+import PhotoOptions from '../components/Sections/PhotoOptions';
 
-const Home: React.FC = () => {
-  return (
-    <>
-      <div className="">
-        <Head>
-          <title>Yummr</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-        </Head>
-        <main className="">
-          <HeroSection />
-          <PhotoOptions />
-          <Features />
-          <CTA />
-        </main>
+const Home: React.FC = () =>
+  <>
+    <div className="">
+      <Head>
+        <title>Yummr</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      </Head>
+      <main className="">
+        <HeroSection />
+        <PhotoOptions />
+        <Features />
+        <CTA />
+      </main>
 
-        <Footer />
-      </div>
-    </>
-  )
-}
-
-export default Home
+      <Footer />
+    </div>
+  </>;
+export default Home;
