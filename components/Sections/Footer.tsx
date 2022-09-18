8c3b49cc0c751cb40852840767b7b72596a23a27
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-09-18 15:56:32 -0400

    refactored footer

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,9 +1,8 @@
-/* This example requires Tailwind CSS v2.0+ */
+import React from 'react'
 import { footerNavigation } from '../reused_variables/variables'
 
-  
-  export default function Footer() {
-    return (
+const Footer: React.FC = () => {
+  return (
       <footer className="bg-white">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
@@ -26,6 +25,7 @@ import { footerNavigation } from '../reused_variables/variables'
           <p className="mt-8 text-center text-base text-gray-400">&copy; 2022 Reality Experience Technologies LLC All rights reserved.</p>
         </div>
       </footer>
-    )
-  }
-  
+  )
+}
+
+export default Footer
