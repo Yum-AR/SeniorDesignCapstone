cd43bddde59a2c8213f85759e9d8cc5fda7a74cb
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-10-30 22:09:22 -0400

    removing linter errors from icons

--- pages/reusableItems/icons/DietaryIcon.tsx
+++ pages/reusableItems/icons/DietaryIcon.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function DietaryIcon() {
-    return (
+  return (
 
-        <svg xmlns="http://www.w3.org/2000/svg" width="78" height="61" viewBox="0 0 78 61">
-            <text id="" data-name="􀥳" fill="#707070" font-size="20" font-family="HelveticaNeue, SF Compact"><tspan x="0" y="19">􀥳</tspan></text>
-        </svg>
-    )
+    <svg xmlns="http://www.w3.org/2000/svg" width="78" height="61" viewBox="0 0 78 61">
+      <text id="" data-name="􀥳" fill="#707070" fontSize="20"
+        fontFamily="HelveticaNeue, SF Compact"><tspan x="0" y="19">􀥳</tspan></text>
+    </svg>
+  );
 }
-
-
